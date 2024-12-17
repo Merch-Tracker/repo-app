f7c6a358ed1d862befe439e557d3a291a2bedb85
nquidox <nquidox@gmail.com>
2024-12-18 00:28:50 +0300

    repo interface extension

--- pkg/types/repo.go
+++ pkg/types/repo.go
@@ -3,6 +3,7 @@ package types
 type Repo interface {
 	Migrate(any) error
 	Create(any) error
+	ReadOne(any, map[string]any) error
 	Update(any, map[string]any) error
 	Delete(any, map[string]any) error
 }
