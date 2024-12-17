44850ff83f65a5b06545ecada5fd375613a82eb6
nquidox <nquidox@gmail.com>
2024-12-17 22:17:26 +0300

    small refactor

--- pkg/helpers/serde.go
+++ pkg/helpers/serde.go
@@ -10,7 +10,7 @@ func SerializeJSON(w http.ResponseWriter, v interface{}) ([]byte, error) {
 	marshal, err := json.Marshal(v)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		log.Error("Serialize error", err)
+		log.WithField("msg", err).Error("Serialize error")
 		return nil, err
 	}
 	return marshal, nil
@@ -20,7 +20,7 @@ func DeserializeJSON(w http.ResponseWriter, data []byte, s interface{}) error {
 	err := json.Unmarshal(data, &s)
 	if err != nil {
 		w.WriteHeader(http.StatusUnprocessableEntity)
-		log.Error("Deserialize error", err)
+		log.WithField("msg", err).Error("Deserialize error")
 		return err
 	}
 	return nil
