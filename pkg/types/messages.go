74c0af5543b8dc61c02e59c70963f8bfeb1bf360
nquidox <nquidox@gmail.com>
2024-12-20 21:32:01 +0300

    update

--- pkg/types/messages.go
+++ pkg/types/messages.go
@@ -27,3 +27,18 @@ const (
 	TokenParsed    = "Token parsed"
 	JwtCreateError = "Jwt creation failed"
 )
+
+// Merchandise messages
+const (
+	MerchMigrationSuccess = "Merchandise migration success"
+	MerchMigrationError   = "Merchandise migration failed"
+	MerchCreateSuccess    = "Merchandise create successfully"
+	MerchCreateError      = "Merchandise creation failed"
+	MerchReadSuccess      = "Merchandise read successfully"
+	MerchReadError        = "Merchandise read failed"
+	MerchUpdateSuccess    = "Merchandise update successfully"
+	MerchUpdateError      = "Merchandise update failed"
+	MerchDeleteSuccess    = "Merchandise delete successfully"
+	MerchDeleteError      = "Merchandise delete failed"
+	MerchSerializeError   = "Merchandise serialize failed"
+)
