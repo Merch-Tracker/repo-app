2536cca86f4c5ae7f6f558725b276e031b6f3407
nquidox <nquidox@gmail.com>
2024-12-26 23:16:50 +0300

    refactor: flexible params

--- pkg/db/update.go
+++ pkg/db/update.go
@@ -1,9 +1,18 @@
 package db
 
+import "fmt"
+
 func (d *DB) Update(model any, params map[string]any) error {
-	result := d.DB.Where("user_uuid = ?", params["user_uuid"]).Updates(model)
-	if result.Error != nil {
-		return result.Error
+	query := d.DB.Model(model)
+
+	for k, v := range params {
+		query = query.Where(fmt.Sprintf("%s = ?", k), v)
+	}
+
+	query = query.Updates(model)
+
+	if query.Error != nil {
+		return query.Error
 	}
 	return nil
 }
