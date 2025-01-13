c0a5f5090773f89c2a3bc9d7ecc679f9fbb8bc69
nquidox <nquidox@gmail.com>
2025-01-13 17:54:51 +0300

    extend: db log level from env

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"repo-app/config"
+	"strings"
 )
 
 type DB struct {
@@ -16,9 +17,24 @@ func Connection(c *config.Config) (*DB, error) {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		c.DBConf.Host, c.DBConf.Port, c.DBConf.Username, c.DBConf.Password, c.DBConf.DBName, c.DBConf.SSLMode)
 
+	level := logger.Info
+
+	switch strings.ToLower(c.DBConf.LogLevel) {
+	case "silent":
+		level = logger.Silent
+	case "error":
+		level = logger.Error
+	case "warn":
+		level = logger.Warn
+	case "info":
+		level = logger.Info
+	default:
+		level = logger.Info
+	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		SkipDefaultTransaction: false,
-		Logger:                 logger.Default.LogMode(logger.Info),
+		Logger:                 logger.Default.LogMode(level),
 	})
 
 	if err != nil {
