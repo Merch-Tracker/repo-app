394511e19c46d6525dbd476723df64436061179e
nquidox <nquidox@gmail.com>
2025-01-15 15:30:26 +0300

    refactor: log messages

---
+++ internal/user/messages.go
@@ -0,0 +1,18 @@
+package user
+
+const (
+	migrationErr = "Users table migration failed"
+
+	errMsg    = "error"
+	respMsg   = "response"
+	noRespMsg = "no response"
+
+	userReadError   = "User read failed"
+	userReadSuccess = "User read successfully"
+
+	userUpdateError   = "User update failed"
+	userUpdateSuccess = "User updated successfully"
+
+	userDeleteError   = "User delete failed"
+	userDeleteSuccess = "User deleted successfully"
+)
