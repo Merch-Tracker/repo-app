db2c70692cc3e2985831a84dea92b524424fd51c
nquidox <nquidox@gmail.com>
2025-01-17 19:50:59 +0300

    small fix

--- internal/images/messages.go
+++ internal/images/messages.go
@@ -1,9 +1,10 @@
 package images
 
 const (
-	receiveImageError = "Receive image error"
-	uploadImageError  = "Upload image error"
-	imageDoesNotExist = "Requested image does not exist"
-	getFromDBError    = "Error getting image from DB"
-	imageFetched      = "Image fetched"
+	imagesTableMigrationError = "Images table migration failed"
+	receiveImageError         = "Receive image error"
+	uploadImageError          = "Upload image error"
+	imageDoesNotExist         = "Requested image does not exist"
+	getFromDBError            = "Error getting image from DB"
+	imageFetched              = "Image fetched"
 )
